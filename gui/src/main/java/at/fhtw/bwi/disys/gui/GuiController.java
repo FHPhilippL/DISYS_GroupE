292fb450e41965d2614d38c1ff647468ca4bfda2
Philipp Labner <wi23b088@technikum-wien.at>
2025-04-27 18:42:30 +0200

    add Functionality for refresh button

--- gui/src/main/java/at/fhtw/bwi/disys/gui/GuiController.java
+++ gui/src/main/java/at/fhtw/bwi/disys/gui/GuiController.java
@@ -1,9 +1,19 @@
 package at.fhtw.bwi.disys.gui;
 
+import com.google.gson.Gson;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class GuiController {
 
@@ -34,11 +44,60 @@ public class GuiController {
     @FXML
     public Label GridUsedText;
 
+    @FXML
+    public Label refreshErrorText;
+
+    @FXML
+    public Label StartHourText;
+
+    private static final Gson gson = new Gson();
+    public TextField StartHourInput;
+    public TextField StartMinuteInput;
+    public TextField EndHourInput;
+    public TextField EndMinuteInput;
 
     @FXML
     protected void onRefreshButtonClick() {
+       try{
+            String urlString = "http://localhost:8080/energy/current";
+            System.out.println(urlString);
 
+            final URLConnection connection = new URL(urlString).openConnection();
+            try (
+                    final InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+                    final BufferedReader br = new BufferedReader(isr)
+
+            ){
+                StringBuilder responseBuilder = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    responseBuilder.append(line);
+                }
+
+                String jsonResponse = responseBuilder.toString();
+                System.out.println("Response JSON: " + jsonResponse);
+
+                // Parse JSON into a Java object
+                ServerResponse response = gson.fromJson(jsonResponse, ServerResponse.class);
+
+                // Update GUI labels
+                CommunityPoolUsageText.setText(String.format("%.2f%% used", response.communityDepleted));
+                GridPortionPercentageText.setText(String.format("%.2f%%", response.gridPortion));
+            }
+       } catch (IOException e) {
+           e.printStackTrace();
+           refreshErrorText.setText("ERROR: " + e.getLocalizedMessage());
+
+       }
     }
+
+    private static class ServerResponse {
+        String hour;
+        double communityDepleted;
+        double gridPortion;
+    }
+
+
     @FXML
     protected void onShowDataButtonClick() {
 
--- gui/src/main/java/module-info.java
+++ gui/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module at.fhtw.bwi.disys.gui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
 
-    opens at.fhtw.bwi.disys.gui to javafx.fxml;
+    opens at.fhtw.bwi.disys.gui to javafx.fxml, com.google.gson;
     exports at.fhtw.bwi.disys.gui;
 }
